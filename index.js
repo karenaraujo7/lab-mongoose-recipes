6845b286fdef9d89b4c7075596901fcb13cf4c7a
Karen <karen_araujp7@hotmail.com>
2021-12-07 03:05:32 -0300

    correção

--- index.js
+++ index.js
@@ -17,44 +17,49 @@ mongoose
   })
   .then(async () => {
     // Run your code here, after you have insured that the connection was made
-    const createRecipe = await Recipe.create({
-      "title": "Asian Glazed Chicken Thighs",
-      "level": "Amateur Chef",
-      "ingredients": [
-        "1/2 cup rice vinegar",
-        "5 tablespoons honey",
-        "1/3 cup soy sauce (such as Silver Swan®)",
-        "1/4 cup Asian (toasted) sesame oil",
-        "3 tablespoons Asian chili garlic sauce",
-        "3 tablespoons minced garlic",
-        "salt to taste",
-        "8 skinless, boneless chicken thighs"
-      ],
-      "cuisine": "Asian",
-      "dishType": "main_course",
-      "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-      "duration": 40,
-      "creator": "Chef LePapu"
-    });
-    console.log(createRecipe.title)
-
-    // Inserindo várias receitas 
-    const insertManyRecipes = await Recipe.insertMany(data)
-
-    // Atualizando 
-    const updatedRecipe = await Recipe.findOneAndUpdate(
-      {title: "Asian Glazed Chicken Thighs"},
-      {$set: {duration: 50}},
-      {new: true}
-    );
-    console.log("Produto atualizado =>", updatedRecipe )
-
-    // Deletando 
-    const deleteRecipes = await Recipe.deleteOne(
-      {title: "Carrot Cake"},
-    );
-
-    console.log('PRODUTO DELETADO =>', deleteRecipes )
+    try {
+      // CRIAR UM DOCUMENTO 
+      const createRecipe = await Recipe.create({
+        "title": "Fricassê de Frango",
+        "level": "Easy Peasy",
+        "ingredients": [
+          "1 peito de frango",
+          "1 pacote de milho",
+          "2cx de creme de leite ",
+          "1 requeijão cremoso",
+          "Batata palha",
+          "Queijo"
+        ],
+        "cuisine": "Brasileiro",
+        "dishType": "main_course",
+        "image": "https://i2.wp.com/www.receitamaneira.com.br/wp-content/uploads/2020/11/fricasse-frango-facil.jpg?w=770&ssl=1",
+        "duration": 40,
+        "creator": "Karen Araujo"
+      });
+      console.log(createRecipe.title)
+  
+      // Inserindo várias receitas 
+      const insertManyRecipes = await Recipe.insertMany(data)
+  
+      // Atualizando 
+      const updatedRecipe = await Recipe.findOneAndUpdate(
+        {title: "Asian Glazed Chicken Thighs"},
+        {$set: {duration: 50}},
+        {new: true}
+      );
+      console.log("Produto atualizado =>", updatedRecipe )
+  
+      // Deletando 
+      const deleteRecipes = await Recipe.deleteOne(
+        {title: "Carrot Cake"},
+      );
+  
+      console.log('PRODUTO DELETADO =>', deleteRecipes )
+      
+    } catch(err){
+      console.log(err)
+    }
+    
 
   })
   .catch(error => {
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -6,7 +6,7 @@ const recipeSchema = new Schema({
   level: {type:  String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"] },
   ingredients: {type: [String]},
   cuisine: {type: String, required: true},
-  dishType: {type: String, enum: ["breakfast", "main_course", "soup", "drink", "dessert", ""]},
+  dishType: {type: String, enum: ["breakfast", "main_course", "soup", "drink", "dessert", "Other"]},
   image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
   duration: {type: Number, min: 0},
   creator: {type: String},
